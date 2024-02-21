16c902841183086a6cba53809110176a2c1dcc3c
Shabab Qaisar <sqaisar@pypestream.com>
2024-02-21 23:31:22 +0500

    Using specific git version

--- src/setup-auto.js
+++ src/setup-auto.js
@@ -19,7 +19,9 @@ async function setupAutoCLI() {
     core.debug(`File downloaded: ${downloadPath}`);
 
     await exec(`gzip -d ${downloadPath} && mv /usr/local/bin/auto-linux /usr/local/bin/auto && chmod +x /usr/local/bin/auto`);
-
+    await exec("sudo apt-get update");
+    await exec("sudo add-apt-repository ppa:git-core/ppa ");
+    await exec("sudo apt install git=1:2.34.1-1ubuntu1.10 git-man=1:2.34.1-1ubuntu1.10 -y --allow-downgrades");
     core.addPath("/usr/local/bin/auto");
     core.info(`Setup finished for auto, version: ${await exec("auto --version").stdout.trim()}`);
 }
